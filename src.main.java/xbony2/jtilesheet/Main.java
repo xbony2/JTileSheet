b64052436f6a72f6c51f11386e6915263500250c
xbony2 <xbony22@gmail.com>
2015-04-12 09:35:12 -0400

    it was all good until it all broke

---
+++ src.main.java/xbony2/jtilesheet/Main.java
@@ -0,0 +1,28 @@
+package xbony2.jtilesheet;
+
+public class Main/* extends Application*/ {
+	public static void main(String[] args){
+		//launch(args);
+	}
+
+	/*@Override
+	public void start(Stage pStage){
+		Button btn = new Button();
+    	btn.setText("Say 'Hello World'");
+    	btn.setOnAction(new EventHandler<ActionEvent>() {
+    		@Override
+    		public void handle(ActionEvent event) {
+    			System.out.println("Hello World!");
+    		}
+    	});
+        
+    	StackPane root = new StackPane();
+    	root.getChildren().add(btn);
+
+    	Scene scene = new Scene(root, 300, 250);
+
+    	pStage.setTitle("Hello World!");
+    	pStage.setScene(scene);
+    	pStage.show();
+	}*/
+}
