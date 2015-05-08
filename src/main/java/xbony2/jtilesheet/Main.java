eed32601fe17e298435a0b24bfc22d8df038da81
xbony2 <xbony22@gmail.com>
2015-05-07 21:01:20 -0400

    okie dokie

--- src/main/java/xbony2/jtilesheet/Main.java
+++ src/main/java/xbony2/jtilesheet/Main.java
@@ -7,6 +7,16 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 
+/**
+ * So, goals for this project:
+ * <ul>
+ * 	<li>The code is written in Java, rather then a very-evolving language like Rust.</li>
+ *  <li>It is GUI-based (JavaFX) rather then command-line based.</li>
+ *  <li>Making tilesheets is possible without bothering Retep.</li>
+ * </ul>
+ * 
+ * @author xbony2
+ */
 public class Main extends Application {
 	public static void main(String[] args){
 		launch(args);
